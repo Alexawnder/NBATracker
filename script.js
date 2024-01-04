6dd9fb92253bf2e058dd9f4dad341d6d54d9600d
Alexander <alexanderlee389@gmail.com>
2024-01-04 11:53:42 -0800

    Complete Update
    Overhaul to the whole UI of the project

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function showInfo(){
-    var playerName = document.getElementById('playerNameInput').value;
+async function showInfo(){
+    var playerName = document.getElementById('search-bar').value;
     var playerID = "0"
-    fetch('https://www.balldontlie.io/api/v1/players?search=' + playerName)
+    await fetch('https://www.balldontlie.io/api/v1/players?search=' + playerName)
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -15,16 +15,15 @@ function showInfo(){
         if (data.data.length > 0) {
             var player = data.data[0];
             playerID = player.id;
+            getPlayerStats(playerID);
             displayStats(playerID);
             // Display specific information in the user interface
             userInfo.innerHTML = `
                 <strong>Player Info:</strong><br>
-                Player ID: ${playerID} <br>
                 Player Name: ${player.first_name} ${player.last_name} <br>
                 Height: ${player.height_feet}'${player.height_inches}" <br>
                 Position: ${player.position} <br>
                 Team: ${player.team.full_name} <br>
-                Weight: ${player.weight_pounds} pounds <br>
             `;
         } else {
             userInfo.textContent = 'No player found.';
@@ -35,8 +34,8 @@ function showInfo(){
     });
 }
 
-function displayStats(playerID){
-    fetch('https://www.balldontlie.io/api/v1/season_averages?seasons[]=2023&player_ids[]=' + playerID)
+async function getPlayerStats(playerID) {
+    await fetch('https://www.balldontlie.io/api/v1/stats?seasons[]=2023&page=2&player_ids[]=' + playerID)
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -44,7 +43,36 @@ function displayStats(playerID){
         return response.json();
     })
     .then(data => {
-        // Process the data
+        const recentGames = data.data.slice(-5); 
+        const totalPoints = recentGames.reduce((sum, game) => sum + game.pts, 0);
+        const averagePoints = totalPoints / recentGames.length;
+        const totalRebounds = recentGames.reduce((sum, game) => sum + game.reb, 0);
+        const averageRebounds = totalRebounds / recentGames.length;
+        const totalAssists = recentGames.reduce((sum, game) => sum + game.ast, 0);
+        const averageAssists = totalAssists / recentGames.length;
+        
+        var recentInfo = document.getElementById("recentInfo");
+        recentInfo.innerHTML = `
+            <strong>Player Info:</strong><br>
+            Average Points Past 5 Games: ${averagePoints} <br>
+            Average Rebounds Past 5 Games: ${averageRebounds} <br>
+            Average Assists Past 5 Games: ${averageAssists} <br>
+        `;
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+async function displayStats(playerID){
+    await fetch('https://www.balldontlie.io/api/v1/season_averages?seasons[]=2023&player_ids[]=' + playerID)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
         var statInfo = document.getElementById("statInfo");
         var player = data.data[0];
         statInfo.innerHTML = `
